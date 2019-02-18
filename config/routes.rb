b15ce0475265030ef421b21c8c9a297212e16194
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-02-18 07:22:43 +0000

    Create a User model(include validation)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
