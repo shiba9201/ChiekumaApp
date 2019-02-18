562750913162f95e6f450c11648c715afa890c95
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-02-18 08:35:16 +0000

    Add sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'users/edit'
 
-  get 'users/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users, only: [:show,:new,:create,:edit,:update,:destroy]
+  
 end
