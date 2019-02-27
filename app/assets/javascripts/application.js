5c4a693a8d212fb7db1153e7cad4b5973f03e946
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-02-27 06:40:44 +0000

    Version Upgrade Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+//= require popper
