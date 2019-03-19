37ac4c1924965f12ec1cb1b3252230bba0b34a84
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-03-19 17:35:28 +0000

    change photo setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAIJHJKHJJBPMSBGXQ'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['chiekuma-photo']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
