e44b46a8bbb63c9b862fb30265008a3f64ae2bf8
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-03-19 17:46:06 +0000

    update carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,11 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAIJHJKHJJBPMSBGXQ',
+    aws_secret_access_key: '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',
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'chiekuma-photo'
+  config.cache_storage = :fog
 end
