dd560d1ce708d0385cf968eb0f9135840ef29749
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-03-19 18:15:25 +0000

    typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,1 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'AKIAIJHJKHJJBPMSBGXQ',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-1'
-  }
 
-  config.fog_directory  = 'chiekuma-photo'
-  config.cache_storage = :fog
-end
