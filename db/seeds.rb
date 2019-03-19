f95bcf5e99a53e8a1850906f384db16f1dc036b0
EC2 Default User <ec2-user@ip-172-31-8-39.ap-southeast-1.compute.internal>
2019-03-19 20:22:27 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name:"チーズ")
+Category.create(name:"タピオカ")
+Category.create(name:"肉")
+Category.create(name:"ラーメン")
+Category.create(name:"韓国")
+Category.create(name:"いちご")
+Category.create(name:"抹茶")
+Category.create(name:"オムレツ")
+Category.create(name:"その他")
